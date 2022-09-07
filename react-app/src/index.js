0d7e4653495a88ee612dd86f52448a6e09758d83
Vincenzo Marsiano <vincenzo.marsiano@thewavestudio.it>
2022-09-07 09:38:08 +0200

    feat: added sass, removed unused code and added style

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
-import './assets/styles/index.css';
+import './assets/styles/index.scss';
 import reportWebVitals from './utils/reportWebVitals';
 
 const root = createRoot(document.getElementById('root'));
