b4fbc9adf7c5ac2af3ee28489ddff44778bfcfca
Vincenzo Marsiano <vincenzo.marsiano@thewavestudio.it>
2022-09-08 14:44:35 +0200

    feat: restyled listing item

--- react-app/src/components/ListingItem.js
+++ react-app/src/components/ListingItem.js
@@ -15,12 +15,12 @@ const ListingItem = (props) => {
 
   return (
     <div className="listing-item" onClick={showMoreModalEmitter} >
-      <span className="listing-item__type">{item.Type}</span>
       <img className="listing-item__poster" src={item.Poster} alt="the item itself" />
       <div className='listing-item__info'>
         <h2 className="listing-item__info__title">{item.Title}</h2>
         {/* <button onClick={changeHandler}>Choose</button> */}
       </div>
+      <span className="listing-item__type">{item.Type}</span>
     </div>
   );
 }
