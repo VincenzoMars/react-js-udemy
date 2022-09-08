50dcd634c7341322758210998751b775e49c392f
Vincenzo Marsiano <vincenzo.marsiano@thewavestudio.it>
2022-09-08 10:48:47 +0200

    feat: modal added isOpen prop to show or hide modal

--- react-app/src/components/ListingModal.js
+++ react-app/src/components/ListingModal.js
@@ -2,7 +2,7 @@ import '../assets/styles/components/listing-modal.scss';
 
 const ListingModal = (props) => {
 
-  const { item, onCloseModal } = props
+  const { item, onCloseModal, isOpen } = props
 
   const closeModalHandler = event => {
     event.preventDefault();
@@ -11,19 +11,18 @@ const ListingModal = (props) => {
     }
   }
 
-  if (!item) {
-    return
-  }
-
   return (
-    <div className="modal-overlay" onClick={closeModalHandler}>
-      <div className='listing-modal'>
-        <h1>modale assurda</h1>
-        <h2 className="listing-modal__title">{item.title}</h2>
-        <p className="listing-modal__desc">{item.description}</p>
-        <img src={item.imageUrl} alt="the item modal itself" />
-      </div>
-    </div>
+    <>
+      {isOpen
+        ? <div className="modal-overlay" onClick={closeModalHandler}>
+          <div className='listing-modal'>
+            <h2 className="listing-modal__title">{item.title}</h2>
+            <p className="listing-modal__desc">{item.description}</p>
+            <img src={item.imageUrl} alt="the item modal itself" />
+          </div>
+        </div>
+        : <></>}
+    </>
   );
 }
 
