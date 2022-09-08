d35666d11f5d9223e6d0e5ea95ad159799e0a26c
Vincenzo Marsiano <vincenzo.marsiano@thewavestudio.it>
2022-09-08 09:55:01 +0200

    feat: added Login component

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,10 +1,12 @@
 import './assets/styles/app.scss';
 
 import Listing from './components/Listing';
+import Login from './components/Login';
 
 const App = () => {
   return (
     <div className="App">
+      <Login></Login>
       <Listing></Listing>
     </div>
   );
