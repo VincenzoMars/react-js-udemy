3af63eaec32f00303fb11cf5ab542608dabda50c
Vincenzo Marsiano <vincenzo.marsiano@thewavestudio.it>
2022-09-09 12:08:10 +0200

    feat(comment): reminder to useCallback

--- react-app/src/components/Listing.js
+++ react-app/src/components/Listing.js
@@ -22,6 +22,10 @@ const Listing = () => {
   const setMovieInModal = (imdbID) => dispatch({ type: 'SET_MOVIE_BY_ID', imdbID })
   const resetMovieInModal = () => dispatch({ type: 'RESET_MOVIE' })
 
+  /* in realtà per non far ri-valutare il componente figlio ad ogni cambio di stato del padre
+    dovremmo usare delle useCallback() anche per le due funzioni subito sopra perchè sono props dell'item 
+    perchè senza useCallback vengono re-inizializzate e quindi la prop le percepisce come 'nuove' 
+    */
   const moviesMemo = useMemo(() => listingState.movies, [listingState.movies])
 
   return (
