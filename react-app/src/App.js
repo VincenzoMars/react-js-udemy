01e42d08d01b16f4c7f323f78baf9e2ee1129e61
Vincenzo Marsiano <vincenzo.marsiano@thewavestudio.it>
2022-09-13 11:30:50 +0200

    feat: deleted login and context provider from app

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,39 +1,12 @@
 import './assets/styles/app.scss';
-import { useState, useEffect } from 'react'
 import Listing from './components/Listing';
-import Login from './components/Login';
-import { ListingContextProvider } from './contexts/Listing';
+
 const App = () => {
 
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  useEffect(() => {
-    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
-
-    if (storedUserLoggedInInformation === '1') {
-      setIsLoggedIn(true);
-    }
-  }, []);
-
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-    localStorage.setItem('isLoggedIn', '1');
-    setIsLoggedIn(true);
-  };
-
-  const logoutHandler = () => {
-    localStorage.removeItem('isLoggedIn');
-    setIsLoggedIn(false);
-  };
-
   return (
-    <ListingContextProvider>
-      <div className="App">
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Listing onLogout={logoutHandler} />}
-      </div>
-    </ListingContextProvider>
+    <div className="App">
+      <Listing />
+    </div>
   );
 }
 
