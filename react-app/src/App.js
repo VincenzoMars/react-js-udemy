3b53f0c8646b9f13f2be5956a0658adc93bc509d
Vincenzo Marsiano <vincenzo.marsiano@thewavestudio.it>
2022-09-13 12:59:20 +0200

    Squashed commit of the following:
    commit a846047f1ae7b6ada87fe16ac102ea06d65d5220
    Author: Vincenzo Marsiano <vincenzo.marsiano@thewavestudio.it>
    Date:   Tue Sep 13 11:46:58 2022 +0200
    
        feat: added router link
    
    commit 4217f6eefaf246eaef53cf30a1bbb39038fe51f3
    Author: Vincenzo Marsiano <vincenzo.marsiano@thewavestudio.it>
    Date:   Tue Sep 13 11:43:44 2022 +0200
    
        feat: added routes on App
    
    commit 01e42d08d01b16f4c7f323f78baf9e2ee1129e61
    Author: Vincenzo Marsiano <vincenzo.marsiano@thewavestudio.it>
    Date:   Tue Sep 13 11:30:50 2022 +0200
    
        feat: deleted login and context provider from app
    
    commit f862ca3c55bd9c620bd742b815b88be57c8b4342
    Author: Vincenzo Marsiano <vincenzo.marsiano@thewavestudio.it>
    Date:   Tue Sep 13 11:28:36 2022 +0200
    
        feat: added react-router-dom dependency
    
    commit 86715bcbce627e89ff8c766295551b6590ce0a3e
    Author: Vincenzo Marsiano <vincenzo.marsiano@thewavestudio.it>
    Date:   Tue Sep 13 11:27:26 2022 +0200
    
        feat: added react-router dependency

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,39 +1,21 @@
+import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import './assets/styles/app.scss';
-import { useState, useEffect } from 'react'
+
 import Listing from './components/Listing';
-import Login from './components/Login';
-import { ListingContextProvider } from './contexts/Listing';
+
+import WelcomePage from './pages/Welcome'
+
 const App = () => {
 
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  useEffect(() => {
-    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
-
-    if (storedUserLoggedInInformation === '1') {
-      setIsLoggedIn(true);
-    }
-  }, []);
-
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-    localStorage.setItem('isLoggedIn', '1');
-    setIsLoggedIn(true);
-  };
-
-  const logoutHandler = () => {
-    localStorage.removeItem('isLoggedIn');
-    setIsLoggedIn(false);
-  };
-
   return (
-    <ListingContextProvider>
-      <div className="App">
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Listing onLogout={logoutHandler} />}
-      </div>
-    </ListingContextProvider>
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<WelcomePage />} />
+          <Route path="/movies" element={<Listing />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
