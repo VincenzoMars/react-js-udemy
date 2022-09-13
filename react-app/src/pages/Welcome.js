a846047f1ae7b6ada87fe16ac102ea06d65d5220
Vincenzo Marsiano <vincenzo.marsiano@thewavestudio.it>
2022-09-13 11:46:58 +0200

    feat: added router link

--- react-app/src/pages/Welcome.js
+++ react-app/src/pages/Welcome.js
@@ -1,8 +1,12 @@
+import { Link } from 'react-router-dom'
+
 const WelcomePage = () => {
 
     return (
         <div className="welcome-page">
             <h1>WELCOME ALABAMA</h1>
+
+            <Link to='/movies'>Go to Movies Catalog!</Link>
         </div>
     );
 }
