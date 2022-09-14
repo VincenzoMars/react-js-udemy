020b8300a62fe1883e3cd3fcf201d65e92cd74f6
Vincenzo Marsiano <vincenzo.marsiano@thewavestudio.it>
2022-09-14 16:09:52 +0200

    Merge branch 'develop'

--- react-app/src/index.tsx
+++ react-app/src/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
@@ -10,11 +9,9 @@ const $root = document.getElementById('root') as Element
 const root = createRoot($root);
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
 
 reportWebVitals();
