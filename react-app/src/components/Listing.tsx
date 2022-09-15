911357bd77cdc91c4514b83b7a5ae43806adfa61
Vincenzo Marsiano <vincenzo.marsiano@thewavestudio.it>
2022-09-15 17:18:20 +0200

    feat: added portal to listing

--- react-app/src/components/Listing.tsx
+++ react-app/src/components/Listing.tsx
@@ -1,35 +1,35 @@
-import { useMemo, useEffect } from 'react'
-import { createPortal } from 'react-dom'
-import '../assets/styles/components/listing.scss';
-import ListingMovie from './ListingMovie'
-import ListingModal from './ListingModal'
-import { useSelector, useDispatch } from 'react-redux'
-import { listingActions } from '../store/listing-slice'
-import { fetchMovies, fetchMoviesByName } from '../store/listing-actions'
-import State from '../types/store/State'
+import { useMemo, useEffect } from "react";
+import "../assets/styles/components/listing.scss";
+import ListingMovie from "./ListingMovie";
+import ListingModal from "./ListingModal";
+import Portal from "./Portal";
+import { useSelector, useDispatch } from "react-redux";
+import { listingActions } from "../store/listing-slice";
+import { fetchMovies, fetchMoviesByName } from "../store/listing-actions";
+import State from "../types/store/State";
 
 // typed dispatch handmade
 import AppDispatch from "../types/store/AppDispatch";
 
 const Listing = () => {
-
-  const dispatch: AppDispatch = useDispatch()
-  const listingState = useSelector((state: State) => state.listing)
+  const dispatch: AppDispatch = useDispatch();
+  const listingState = useSelector((state: State) => state.listing);
 
   useEffect(() => {
-    dispatch(fetchMovies())
+    dispatch(fetchMovies());
   }, [dispatch]);
 
   const searchMoviesByName = (event: React.FormEvent<HTMLInputElement>) => {
-    debugger
-    const movieName = event.currentTarget.value
-    dispatch(fetchMoviesByName(movieName))
-  }
+    debugger;
+    const movieName = event.currentTarget.value;
+    dispatch(fetchMoviesByName(movieName));
+  };
 
-  const setMovieInModal = (imdbID: string) => dispatch(listingActions.setMovieById({ imdbID }))
-  const resetMovieInModal = () => dispatch(listingActions.resetMovie())
+  const setMovieInModal = (imdbID: string) =>
+    dispatch(listingActions.setMovieById({ imdbID }));
+  const resetMovieInModal = () => dispatch(listingActions.resetMovie());
 
-  const moviesMemo = useMemo(() => listingState.movies, [listingState.movies])
+  const moviesMemo = useMemo(() => listingState.movies, [listingState.movies]);
 
   return (
     <>
@@ -38,22 +38,27 @@ const Listing = () => {
         <input type="text" onChange={searchMoviesByName} />
         <h2 className="listing__category-title">Movies</h2>
         <div className="listing__category-items">
-          {moviesMemo.map((movie) =>
+          {moviesMemo.map((movie) => (
             <ListingMovie
               key={movie && movie.imdbID}
               movie={movie}
               onShowMoreModal={setMovieInModal}
             />
-          )}
+          ))}
         </div>
       </div>
-      {createPortal(<ListingModal
-        movie={listingState.modal.movie}
-        isOpen={listingState.modal.isOpen}
-        onCloseModal={resetMovieInModal}
-      />, document.getElementById('modal-root') as Element)}
+      <Portal
+        children={
+          <ListingModal
+            movie={listingState.modal.movie}
+            isOpen={listingState.modal.isOpen}
+            onCloseModal={resetMovieInModal}
+          />
+        }
+        target="modal-root"
+      />
     </>
-  )
-}
+  );
+};
 
 export default Listing;
