b4af54802b7b177b69a0135b18ea7bed036d3464
Vincenzo Marsiano <vincenzo.marsiano@thewavestudio.it>
2022-09-15 12:34:46 +0200

    feat: added alternative template to listing movie if props are invalid

--- react-app/src/components/ListingMovie.tsx
+++ react-app/src/components/ListingMovie.tsx
@@ -6,24 +6,27 @@ const ListingMovie = (props: ListingMovieProps) => {
 
   const { movie, onShowMoreModal } = props
   
-  if(movie) {
-
-    const showMoreModalEmitter = () => {
-      onShowMoreModal(movie.imdbID)
-    }
-
+  if(!movie) {
     return (
-      <div className="listing-item" onClick={showMoreModalEmitter} >
-        <img className="listing-item__poster" src={movie.Poster} alt="the item itself" />
-        <div className='listing-item__info'>
-          <h2 className="listing-item__info__title">{movie.Title}</h2>
-        </div>
-        <span className="listing-item__type">{movie.Type}</span>
+      <div className="listing-item">
+        <h2 className="listing-item__not-found">Movie Not Found!</h2>
       </div>
-    );
+    )
   }
 
-  return <></>
+  const showMoreModalEmitter = () => {
+    onShowMoreModal(movie.imdbID)
+  }
+
+  return (
+    <div className="listing-item" onClick={showMoreModalEmitter} >
+      <img className="listing-item__poster" src={movie.Poster} alt="the item itself" />
+      <div className='listing-item__info'>
+        <h2 className="listing-item__info__title">{movie.Title}</h2>
+      </div>
+      <span className="listing-item__type">{movie.Type}</span>
+    </div>
+  );
 
 }
 
