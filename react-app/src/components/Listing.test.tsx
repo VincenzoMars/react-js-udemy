941c54f409590100d8bcaf3c72eb40bea2577ae2
Vincenzo Marsiano <vincenzo.marsiano@thewavestudio.it>
2022-09-15 17:17:40 +0200

    feat: listing test update

--- react-app/src/components/Listing.test.tsx
+++ react-app/src/components/Listing.test.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { createPortal } from 'react-dom'
-import store from '../store'
-import { render, screen } from '@testing-library/react';
-import Listing from './Listing';
-import '@testing-library/jest-dom'
+import React from "react";
+import { Provider } from "react-redux";
+import store from "../store";
+import { render, screen } from "@testing-library/react";
+import Listing from "./Listing";
+import "@testing-library/jest-dom";
 
-
-describe('Listing', () => {
-  test('test if initial movies are rendered', async () => {
+describe("Listing", () => {
+  test("if initial movies are rendered", async () => {
     // Arrange
-    render(<>
+    render(
       <Provider store={store}>
         <Listing />
       </Provider>
-      <div id='modal-root'></div>
-    </>, { container: document.body });
+    );
     // Act
     // nothing...
-  
+
     // Assert
-    const headingElement = await screen.findAllByRole(/listing__category-items/i)
+    const headingElement = await screen.findAllByRole(
+      /listing__category-items/i
+    );
     expect(headingElement).not.toHaveLength(0);
   });
-})
+});
