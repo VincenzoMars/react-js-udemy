89a30633edd5d7d5d42709559b0a2803171cead4
Vincenzo Marsiano <vincenzo.marsiano@thewavestudio.it>
2022-09-16 11:38:58 +0200

    feat: formatted Welcome

--- react-app/src/pages/welcome/Welcome.tsx
+++ react-app/src/pages/welcome/Welcome.tsx
@@ -1,15 +1,16 @@
-import { Link } from 'react-router-dom'
-import styles from './Welcome.module.scss'
+import { Link } from "react-router-dom";
+import styles from "./Welcome.module.scss";
 
 const WelcomePage = () => {
-
-    return (
-        <div className={styles.pageWrapper}>
-            <h2 className={styles.subTitle}>Welcome to</h2>
-            <h1 className={styles.title}>WHATCH!</h1>
-            <Link to='/movies' className={styles.subTitleSecondary}>Run to Catalog</Link>
-        </div>
-    );
-}
+  return (
+    <div className={styles.pageWrapper}>
+      <h2 className={styles.subTitle}>Welcome to</h2>
+      <h1 className={styles.title}>WHATCH!</h1>
+      <Link to="/movies" className={styles.subTitleSecondary}>
+        Run to Catalog
+      </Link>
+    </div>
+  );
+};
 
 export default WelcomePage;
