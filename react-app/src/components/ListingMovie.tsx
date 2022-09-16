0f4059a3cb7ee8ebf48e39578a850cd8d021e4a2
Vincenzo Marsiano <vincenzo.marsiano@thewavestudio.it>
2022-09-16 11:38:08 +0200

    fix: ListingMovie formatting

--- react-app/src/components/ListingMovie.tsx
+++ react-app/src/components/ListingMovie.tsx
@@ -1,33 +1,35 @@
-import '../assets/styles/components/listing-item.scss';
-import { memo } from 'react'
-import ListingMovieProps from '../types/props/ListingMovieProps';
+import "../assets/styles/components/listing-item.scss";
+import { memo } from "react";
+import ListingMovieProps from "../types/props/ListingMovieProps";
 
 const ListingMovie = (props: ListingMovieProps) => {
+  const { movie, onShowMoreModal } = props;
 
-  const { movie, onShowMoreModal } = props
-  
-  if(!movie) {
+  if (!movie) {
     return (
       <div className="listing-item">
         <h2 className="listing-item__not-found">Movie Not Found!</h2>
       </div>
-    )
+    );
   }
 
   const showMoreModalEmitter = () => {
-    onShowMoreModal(movie.imdbID)
-  }
+    onShowMoreModal(movie.imdbID);
+  };
 
   return (
-    <div className="listing-item" onClick={showMoreModalEmitter} >
-      <img className="listing-item__poster" src={movie.Poster} alt="the item itself" />
-      <div className='listing-item__info'>
+    <li className="listing-item" onClick={showMoreModalEmitter}>
+      <img
+        className="listing-item__poster"
+        src={movie.Poster}
+        alt="the item itself"
+      />
+      <div className="listing-item__info">
         <h2 className="listing-item__info__title">{movie.Title}</h2>
       </div>
       <span className="listing-item__type">{movie.Type}</span>
-    </div>
+    </li>
   );
-
-}
+};
 
 export default memo(ListingMovie);
